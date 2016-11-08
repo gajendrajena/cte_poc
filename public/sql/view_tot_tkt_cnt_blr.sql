ddcff5bc89f4d67e88beeb4ac88b9047cdf821da
sbarik3 <sbarik@ctepl.com>
2016-11-08 11:01:56 +0530

    Update view_tot_tkt_cnt_blr.sql

--- public/sql/view_tot_tkt_cnt_blr.sql
+++ public/sql/view_tot_tkt_cnt_blr.sql
@@ -2,36 +2,62 @@
 ===============================
 create or replace view v_tkt_cnt_yearwise
 as
-select fin_year "Financial Year",count(no_pers) "Total Tickets" from tkt_dtl
-where branch_cd=2
-group by fin_year
-order by fin_year;
+select b.fin_year "Financial Year",count(a.no_pers) "Total Tickets" 
+from tkt_dtl a,tkt_hdr b
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2
+group by b.fin_year
+order by b.fin_year;
 
 2- monthly data for all these years
 ===================================
 create or replace view v_tkt_cnt_monthwise
 as
-select fin_year "Financial Year",to_char(trans_dt,'MON-RRRR') Months,count(no_pers) "Total Tickets" from tkt_dtl
-where branch_cd=2
-group by fin_year,to_char(trans_dt,'MON-RRRR')
-order by fin_year,to_char(trans_dt,'MON-RRRR');
+select b.fin_year "Financial Year",to_char(b.trans_dt,'MON') Months,count(a.no_pers) "Total Tickets" 
+from tkt_dtl a,tkt_hdr b
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2
+group by b.fin_year,to_char(b.trans_dt,'MON')
+order by b.fin_year,to_char(b.trans_dt,'MON');
 
 3- Weekly data for all these years
 ====================================
 create or replace view v_tkt_cnt_weekly
 as
-select fin_year as YEAR,to_char(trans_dt,'MON') Months,to_char(trans_dt,'W') "WEEK",count(no_pers) "Total Tickets"
-from tkt_dtl
-where branch_cd=2
-group by fin_year, to_char(trans_dt,'MON'), to_char(trans_dt,'W')
+select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",count(a.no_pers) "Total Tickets"
+from tkt_dtl a,tkt_hdr b
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2
+group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'W')
 order by Months;
 
 3- Daily data for all these years
 ====================================
 create or replace view v_tkt_cnt_Daily
 as
-select fin_year as YEAR,to_char(trans_dt,'MON') Months,to_char(trans_dt,'DD') "Daily",count(no_pers) "Total Tickets"
-from tkt_dtl
-where branch_cd=2
-group by fin_year, to_char(trans_dt,'MON'), to_char(trans_dt,'DD')
-order by fin_year;
+select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",count(a.no_pers) "Total Tickets"
+from tkt_dtl a,tkt_hdr b
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2
+group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'DD')
+order by b.fin_year;
