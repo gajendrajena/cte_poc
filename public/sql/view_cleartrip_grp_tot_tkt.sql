009b53cc13a80ec1cca2b60b3629d1624323b40b
sbarik3 <sbarik@ctepl.com>
2016-11-11 15:00:04 +0530

    Create view_cleartrip_grp_tot_tkt.sql

---
+++ public/sql/view_cleartrip_grp_tot_tkt.sql
@@ -0,0 +1,71 @@
+Yearly cleartrip booking data 
+=============================
+create or replace view v_tkt_cnt_clrtrip_yearly
+as
+select b.fin_year,v.vgrp_desc "Group_name",count(no_pers) tickets
+from tkt_dtl a,tkt_hdr b,visit_group v
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2 and 
+b.vgrp_cd=v.vgrp_cd and
+b.CTRIP_NO is not null
+group by b.fin_year,v.vgrp_desc
+order by b.fin_year;
+
+Monthly cleartrip booking data 
+=============================
+create or replace view v_tkt_cnt_clrtrip_monthly
+as
+select b.fin_year,to_char(b.trans_dt,'MON') Months,v.vgrp_desc "Group_name",count(no_pers) tickets
+from tkt_dtl a,tkt_hdr b,visit_group v
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2 and 
+b.vgrp_cd=v.vgrp_cd and
+b.CTRIP_NO is not null
+group by b.fin_year,to_char(b.trans_dt,'MON'),v.vgrp_desc
+order by b.fin_year;
+
+Weekly cleartrip booking data 
+=============================
+create or replace view v_tkt_cnt_clrtrip_weekly
+as
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",v.vgrp_desc "Group_name",count(no_pers) tickets
+from tkt_dtl a,tkt_hdr b,visit_group v
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2 and 
+b.vgrp_cd=v.vgrp_cd and
+b.CTRIP_NO is not null
+group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'W'),v.vgrp_desc
+order by b.fin_year;
+
+Daily cleartrip booking data 
+=============================
+create or replace view v_tkt_cnt_clrtrip_daily
+as
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",v.vgrp_desc "Group_name",count(no_pers) tickets
+from tkt_dtl a,tkt_hdr b,visit_group v
+where a.FIN_YEAR=b.FIN_YEAR and
+a.BRANCH_CD=b.BRANCH_CD and
+a.POS_POINT_CD=b.POS_POINT_CD and
+a.SUB_CD=b.SUB_CD and
+a.REC_NO=b.REC_NO and
+a.DAY_CD=b.DAY_CD and
+a.branch_Cd=2 and 
+b.vgrp_cd=v.vgrp_cd and
+b.CTRIP_NO is not null
+group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'DD'),v.vgrp_desc
+order by b.fin_year;
