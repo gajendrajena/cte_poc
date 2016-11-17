b1616708349e8b8ec41f0b1f6c09878dc4d06295
sbarik3 <sbarik@ctepl.com>
2016-11-17 18:27:40 +0530

    Update view_walkin_grp_tot_tkt_cnt.sql

--- public/sql/view_walkin_grp_tot_tkt_cnt.sql
+++ public/sql/view_walkin_grp_tot_tkt_cnt.sql
@@ -2,7 +2,7 @@ Yearwise Wlak-in group Data
 ===========================
 create or replace view v_tkt_cnt_walkin_yearly
 as
-select b.fin_year,v.vgrp_desc "Group_name",count(no_pers) tickets
+select b.fin_year,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -10,7 +10,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9) and
 b.vgrp_cd=v.vgrp_cd
 group by b.fin_year,v.vgrp_desc
@@ -20,7 +19,7 @@ MOnthwise Wlak-in group Data
 ============================
 create or replace view v_tkt_cnt_walkin_monthly
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,v.vgrp_desc "Group_name",count(no_pers) tickets
+select b.fin_year,to_char(a.trans_dt,'MON') Months,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -28,7 +27,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9) and
 b.vgrp_cd=v.vgrp_cd
 group by b.fin_year,to_char(a.trans_dt,'MON'),v.vgrp_desc
@@ -38,7 +36,7 @@ Weekly Wlak-in group Data
 ============================
 create or replace view v_tkt_cnt_walkin_weekly
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'W') "WEEK",v.vgrp_desc "Group_name",count(no_pers) tickets
+select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'W') "WEEK",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -46,7 +44,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9) and
 b.vgrp_cd=v.vgrp_cd
 group by b.fin_year,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'W'),v.vgrp_desc
@@ -56,7 +53,7 @@ Daily Wlak-in group Data
 ============================
 create or replace view v_tkt_cnt_walkin_daily
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'DD') "Daily",v.vgrp_desc "Group_name",count(no_pers) tickets
+select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'DD') "Daily",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -64,7 +61,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9) and
 b.vgrp_cd=v.vgrp_cd
 group by b.fin_year,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'DD'),v.vgrp_desc
