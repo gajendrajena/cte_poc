6d071290044a68bf2315d60c78429a5bb3f1fd8b
sbarik3 <sbarik@ctepl.com>
2016-11-17 18:13:28 +0530

    Update tot_walk-in_bkng.sql

--- public/sql/tot_walk-in_bkng.sql
+++ public/sql/tot_walk-in_bkng.sql
@@ -2,7 +2,7 @@ yearwise total walk_in Booking
 ==============================
 create or replace view v_tot_walkin_yearly
 as
-select b.fin_year,count(no_pers) tickets
+select b.fin_year,nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -10,7 +10,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9)
 group by b.fin_year
 order by b.fin_year;
@@ -19,7 +18,7 @@ monthwise total walk_in Booking
 ===============================
 create or replace view v_tot_walkin_monthly
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -27,7 +26,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9)
 group by b.fin_year,to_char(b.trans_dt,'MON')
 order by b.fin_year;
@@ -36,7 +34,7 @@ Weekly total Wlak-in Data
 =========================
 create or replace view v_tot_walkin_weekly
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -44,7 +42,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9)
 group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'W')
 order by b.fin_year;
@@ -53,7 +50,7 @@ Daily total Wlak-in  Data
 =========================
 create or replace view v_tot_walkin_daily
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -61,7 +58,6 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
 b.pay_cd not in (6,9)
 group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'DD')
 order by b.fin_year;
