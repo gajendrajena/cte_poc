f348e87123059adcf6136a47cb9f9ae6f926bd9b
sbarik3 <sbarik@ctepl.com>
2016-11-17 18:24:56 +0530

    Update and rename view_tot_tkt_cnt_blr.sql to view_tot_tkt_cnt.sql

--- public/sql/view_tot_tkt_cnt.sql
+++ public/sql/view_tot_tkt_cnt.sql
@@ -2,15 +2,14 @@
 ===============================
 create or replace view v_tkt_cnt_yearwise
 as
-select b.fin_year "Financial Year",count(a.no_pers) "Total Tickets" 
+select b.fin_year "Financial Year",nvl(count(no_pers),0) "Total Tickets" 
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2
+a.DAY_CD=b.DAY_CD 
 group by b.fin_year
 order by b.fin_year;
 
@@ -18,15 +17,14 @@ order by b.fin_year;
 ===================================
 create or replace view v_tkt_cnt_monthwise
 as
-select b.fin_year "Financial Year",to_char(b.trans_dt,'MON') Monthly,count(a.no_pers) "Total Tickets" 
+select b.fin_year "Financial Year",to_char(b.trans_dt,'MON') Monthly,nvl(count(no_pers),0) "Total Tickets" 
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2
+a.DAY_CD=b.DAY_CD
 group by b.fin_year,to_char(b.trans_dt,'MON')
 order by b.fin_year,to_char(b.trans_dt,'MON');
 
@@ -34,15 +32,14 @@ order by b.fin_year,to_char(b.trans_dt,'MON');
 ====================================
 create or replace view v_tkt_cnt_weekly
 as
-select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "Weekly",count(a.no_pers) "Total Tickets"
+select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "Weekly",nvl(count(no_pers),0) "Total Tickets"
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2
+a.DAY_CD=b.DAY_CD
 group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'W')
 order by Months;
 
@@ -50,14 +47,13 @@ order by Months;
 ====================================
 create or replace view v_tkt_cnt_Daily
 as
-select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",count(a.no_pers) "Total Tickets"
+select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) "Total Tickets"
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2
+a.DAY_CD=b.DAY_CD
 group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'DD')
 order by b.fin_year;
