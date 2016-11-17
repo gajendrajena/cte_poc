3eef8eed6991a17795ff8b509d797ddc4779bddd
sbarik3 <sbarik@ctepl.com>
2016-11-17 18:09:46 +0530

    Update tot_ctrip_bkng.sql

--- public/sql/tot_ctrip_bkng.sql
+++ public/sql/tot_ctrip_bkng.sql
@@ -2,16 +2,14 @@ yearwise total cleartrip Booking
 ================================
 create or replace view v_tot_ctrip_yearly
 as
-select b.fin_year,count(no_pers) tickets
+select b.fin_year,nvl(count(ctrip_no),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and
-b.CTRIP_NO is not null
+a.DAY_CD=b.DAY_CD
 group by b.fin_year
 order by b.fin_year;
 
@@ -19,16 +17,14 @@ Monthly total cleartrip booking data
 ====================================
 create or replace view v_tot_ctrip_monthly
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,nvl(count(ctrip_no),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
-b.CTRIP_NO is not null
+a.DAY_CD=b.DAY_CD
 group by b.fin_year,to_char(b.trans_dt,'MON')
 order by b.fin_year;
 
@@ -36,16 +32,14 @@ Weekly total cleartrip booking data
 ==================================
 create or replace view v_tot_ctrip_weekly
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",nvl(count(ctrip_no),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
-b.CTRIP_NO is not null
+a.DAY_CD=b.DAY_CD
 group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'W')
 order by b.fin_year;
 
@@ -53,15 +47,13 @@ Daily cleartrip booking data
 =============================
 create or replace view v_tot_ctrip_daily
 as
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",count(no_pers) tickets
+select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(ctrip_no),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
 a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
-a.DAY_CD=b.DAY_CD and
-a.branch_Cd=2 and 
-b.CTRIP_NO is not null
+a.DAY_CD=b.DAY_CD
 group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'DD')
 order by b.fin_year;
