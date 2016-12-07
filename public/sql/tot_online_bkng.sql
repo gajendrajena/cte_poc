4d7d3cb10de6857acf83b9803df7f257c314242c
asanikommu <asanikommu@ctepl.com>
2016-12-07 18:19:25 +0530

    Update tot_online_bkng.sql

--- public/sql/tot_online_bkng.sql
+++ public/sql/tot_online_bkng.sql
@@ -2,7 +2,7 @@ yearwise total online Booking
 =============================
 create or replace view v_tot_online_yearly
 as
-select b.fin_year,nvl(count(no_pers),0) tickets
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -10,10 +10,11 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
-b.pay_cd in (6,9)
-group by b.fin_year
-union
-select b.fin_year,nvl2(count(no_pers),0,0) tickets
+B.PAY_CD IN (6,9)
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,B.FIN_YEAR
+
+UNION
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,nvl2(count(no_pers),0,0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -24,15 +25,15 @@ a.DAY_CD=b.DAY_CD and
 b.pay_cd not in (6,9) 
 and (b.fin_year) not in (select fin_year
                           from tkt_hdr
-                          where pay_cd in (6,9))
-group by b.fin_year 
-order by fin_year;
+                          WHERE PAY_CD IN (6,9))
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'),B.FIN_YEAR 
+order by "Branch Name",fin_year;
 
 monthwise total online Booking
 ==============================
 create or replace view v_tot_online_monthly
 as
-select all b.fin_year,to_char(b.trans_dt,'MON') Months,nvl(count(no_pers),0) tickets
+select all decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -41,9 +42,9 @@ a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9)
-group by b.fin_year,to_char(b.trans_dt,'MON')
-Union
-select b.fin_year,to_char(b.trans_dt,'MON') Months,nvl2(count(no_pers),0,0) tickets
+GROUP BY decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'), b.fin_year, to_char(b.trans_dt,'MON')
+UNION
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,nvl2(count(no_pers),0,0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -54,15 +55,15 @@ a.DAY_CD=b.DAY_CD and
 b.pay_cd not in (6,9) 
 and (b.fin_year,to_char(b.trans_dt,'MON')) not in (select fin_year,to_char(trans_dt,'MON')
                                                     from tkt_hdr
-                                                    where pay_cd in (6,9))
-group by b.fin_year,to_char(b.trans_dt,'MON') 
-order by fin_year;
+                                                    WHERE PAY_CD IN (6,9))
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,B.FIN_YEAR,TO_CHAR(B.TRANS_DT,'MON') 
+order by "Branch Name",fin_year;
 
 Weekly total online Data
 =========================
 create or replace view v_tot_online_weekly
 as
-select all b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",nvl(count(no_pers),0) tickets
+select all decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "WEEK",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -71,9 +72,9 @@ a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9)
-group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'W')
-Union
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W'),nvl2(count(no_pers),0,0) tickets
+GROUP BY decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'), b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'W')
+UNION
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W'),nvl2(count(no_pers),0,0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -84,16 +85,16 @@ a.DAY_CD=b.DAY_CD and
 b.pay_cd not in (6,9) 
 and (b.fin_year,to_char(b.trans_dt,'MON')) not in (select fin_year,to_char(trans_dt,'MON')
                                                     from tkt_hdr
-                                                    where pay_cd in (6,9))
-group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'W')
-order by fin_year;
+                                                    WHERE PAY_CD IN (6,9))
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,B.FIN_YEAR,TO_CHAR(B.TRANS_DT,'MON'),TO_CHAR(B.TRANS_DT,'W')
+order by "Branch Name",fin_year;
 
 
 Daily total online booking  Data
 ================================
 create or replace view v_tot_online_daily
 as
-select all b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) tickets
+select all decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -102,9 +103,9 @@ a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9)
-group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'DD')
-Union
-select b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl2(count(no_pers),0,0) tickets
+GROUP BY decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'), b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'DD')
+UNION
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl2(count(no_pers),0,0) tickets
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -115,6 +116,6 @@ a.DAY_CD=b.DAY_CD and
 b.pay_cd not in (6,9) 
 and (b.fin_year,to_char(b.trans_dt,'MON')) not in (select fin_year,to_char(trans_dt,'MON')
                                                     from tkt_hdr
-                                                    where pay_cd in (6,9))
-group by b.fin_year,to_char(b.trans_dt,'MON'),to_char(b.trans_dt,'DD')
-order by fin_year;
+                                                    WHERE PAY_CD IN (6,9))
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,B.FIN_YEAR,TO_CHAR(B.TRANS_DT,'MON'),TO_CHAR(B.TRANS_DT,'DD')
+order by "Branch Name",fin_year;
