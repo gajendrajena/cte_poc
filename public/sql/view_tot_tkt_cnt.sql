504dfd153f3df2f2589e255997f9ac7b9f86dbf6
asanikommu <asanikommu@ctepl.com>
2016-12-08 09:56:11 +0530

    Update view_tot_tkt_cnt.sql

--- public/sql/view_tot_tkt_cnt.sql
+++ public/sql/view_tot_tkt_cnt.sql
@@ -2,7 +2,7 @@
 ===============================
 create or replace view v_tkt_cnt_yearwise
 as
-select b.fin_year "Financial Year",nvl(count(no_pers),0) "Total Tickets" 
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year "Financial Year",nvl(count(no_pers),0) "Total Tickets" 
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -10,14 +10,14 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD 
-group by b.fin_year
-order by b.fin_year;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year;
 
 2- monthly data for all these years
 ===================================
 create or replace view v_tkt_cnt_monthwise
 as
-select b.fin_year "Financial Year",to_char(b.trans_dt,'MON') Monthly,nvl(count(no_pers),0) "Total Tickets" 
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year "Financial Year",to_char(b.trans_dt,'MON') Monthly,nvl(count(no_pers),0) "Total Tickets" 
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -25,14 +25,14 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD
-group by b.fin_year,to_char(b.trans_dt,'MON')
-order by b.fin_year,to_char(b.trans_dt,'MON');
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year,to_char(b.trans_dt,'MON')
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year,to_char(b.trans_dt,'MON');
 
 3- Weekly data for all these years
 ====================================
 create or replace view v_tkt_cnt_weekly
 as
-select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "Weekly",nvl(count(no_pers),0) "Total Tickets"
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'W') "Weekly",nvl(count(no_pers),0) "Total Tickets"
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -40,14 +40,14 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD
-group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'W')
-order by Months;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'W')
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,Months;
 
 3- Daily data for all these years
 ====================================
 create or replace view v_tkt_cnt_Daily
 as
-select b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) "Total Tickets"
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year as YEAR,to_char(b.trans_dt,'MON') Months,to_char(b.trans_dt,'DD') "Daily",nvl(count(no_pers),0) "Total Tickets"
 from tkt_dtl a,tkt_hdr b
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -55,5 +55,5 @@ a.POS_POINT_CD=b.POS_POINT_CD and
 a.SUB_CD=b.SUB_CD and
 a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD
-group by b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'DD')
-order by b.fin_year;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year, to_char(b.trans_dt,'MON'), to_char(b.trans_dt,'DD')
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year;
