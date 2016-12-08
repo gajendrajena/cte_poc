7e86d63afd28eec46fb39310401a3f79b979f5f9
asanikommu <asanikommu@ctepl.com>
2016-12-08 09:54:17 +0530

    Update view_onlinetrip_grp_tot_tkt.sql

--- public/sql/view_onlinetrip_grp_tot_tkt.sql
+++ public/sql/view_onlinetrip_grp_tot_tkt.sql
@@ -2,7 +2,7 @@ Yearly online booking data
 =============================
 create or replace view v_tkt_cnt_online_yearly
 as
-select b.fin_year,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -12,14 +12,14 @@ a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9) and
 b.vgrp_cd=v.vgrp_cd
-group by b.fin_year,v.vgrp_desc
-order by b.fin_year;
+GROUP BY DECODE(B.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'), B.FIN_YEAR, V.VGRP_DESC
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad'),b.fin_year;
 
 monthly online booking data 
 =============================
 create or replace view v_tkt_cnt_online_monthly
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(a.trans_dt,'MON') Months,v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -29,14 +29,14 @@ a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9) and
 b.vgrp_cd=v.vgrp_cd
-group by b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON') 
-order by b.fin_year;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON') 
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year;
 
 weekly online booking data 
 ==========================
 create or replace view v_tkt_cnt_online_weekly
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'W') "WEEK",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'W') "WEEK",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -46,14 +46,14 @@ a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9) and
 b.vgrp_cd=v.vgrp_cd
-group by b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'W') 
-order by b.fin_year;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'W') 
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year;
 
 Daily online booking data 
 ==========================
 create or replace view v_tkt_cnt_online_daily
 as
-select b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'DD') "Daily",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
+select decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') "Branch Name",b.fin_year,to_char(a.trans_dt,'MON') Months,to_char(a.trans_dt,'DD') "Daily",v.vgrp_desc "Group_name",nvl(count(no_pers),0) tickets
 from tkt_dtl a,tkt_hdr b,visit_group v
 where a.FIN_YEAR=b.FIN_YEAR and
 a.BRANCH_CD=b.BRANCH_CD and
@@ -63,5 +63,5 @@ a.REC_NO=b.REC_NO and
 a.DAY_CD=b.DAY_CD and
 b.pay_cd in (6,9) and
 b.vgrp_cd=v.vgrp_cd
-group by b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'DD')
-order by b.fin_year;
+group by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year,v.vgrp_desc,to_char(a.trans_dt,'MON'),to_char(a.trans_dt,'DD')
+order by decode(b.BRANCH_CD,1,'Kochi',2,'Bangalore','Hyderabad') ,b.fin_year;
