d0907480973625fa320b134cf619c23ce28bdd6b
Gajendra Jena <gjena@ctepl.com>
2017-01-05 19:11:12 +0530

    adding app constants

---
+++ config/initializers/app_constants.rb
@@ -0,0 +1,4 @@
+BRANCHES = { '1' => 'Wonderla Hoildays Ltd,Kochi', '2' => 'Wonderla Holidays Ltd,Bangalore', '4' => 'Wonderla Holidays Ltd,Hyderabad'}
+USER_GROUPS = {'1' => "Adult", '2' => "Child"}
+DAY_GROUPS = {'1' => "Peakdays", '2' => "Weekdays"}
+BOOKING_SRC = {'1' => "Walkin", '2' => "Online", '3' => "Cleartrip"}
