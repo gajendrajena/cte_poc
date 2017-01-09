70ed5be1b2baf8b68364a5f8fb4bad710b063986
Gajendra Jena <gjena@ctepl.com>
2017-01-09 13:25:00 +0530

    added index and removed unwanted tables

---
+++ db/migrate/20170109073716_add_index_to_all.rb
@@ -0,0 +1,25 @@
+class AddIndexToAll < ActiveRecord::Migration[5.0]
+  def change
+    add_index :tkt_cnt_daywises, :branch_id
+    add_index :tkt_cnt_monthwises, :branch_id
+    add_index :tkt_cnt_weekwises, :branch_id
+    add_index :tkt_cnt_yearwises, :branch_id
+    add_index :weekwise_booking_sources, :branch_id
+    add_index :weekwise_peekday_weekdays, :branch_id
+    add_index :yearwise_booking_sources, :branch_id
+    add_index :yearwise_peekday_weekdays, :branch_id
+
+    add_index :tkt_cnt_daywises, :fin_year
+    add_index :tkt_cnt_monthwises, :fin_year
+    add_index :tkt_cnt_weekwises, :fin_year
+    add_index :tkt_cnt_yearwises, :fin_year
+    add_index :weekwise_booking_sources, :fin_year
+    add_index :yearwise_booking_sources, :fin_year
+    add_index :weekwise_peekday_weekdays, :fin_year
+    add_index :yearwise_peekday_weekdays, :fin_year
+
+    add_index :weekwise_booking_sources, :booking_src_id
+    add_index :yearwise_booking_sources, :booking_src_id
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,110 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105121702) do
+ActiveRecord::Schema.define(version: 20170109075224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "agents", force: :cascade do |t|
-    t.string "admin_id"
-    t.string "agency_name"
-    t.string "accounting_name"
-    t.string "first_name"
-    t.string "middle_name"
-    t.string "last_name"
-    t.string "agent_type"
-    t.string "section_name"
-    t.string "credit_limit"
-    t.string "credit_value"
-    t.string "credit_usage"
-    t.string "credit_distribution"
-    t.string "opening_balance"
-    t.string "lead_time"
-    t.string "address"
-    t.string "city"
-    t.string "pincode"
-    t.string "country"
-    t.string "time_zone_offset"
-    t.string "enable_dst"
-    t.string "phone"
-    t.string "mobile"
-    t.string "fax"
-    t.string "email"
-    t.string "username"
-    t.string "password"
-    t.string "designation"
-    t.string "nature_of_business"
-    t.string "logo"
-    t.string "currency"
-    t.string "credit_option"
-    t.string "no_of_days"
-    t.string "status"
-    t.string "create_date"
-    t.string "modify_date"
-    t.string "parent_agent"
-    t.string "debug"
-    t.string "can_book"
-    t.string "can_voucher"
-    t.string "markup"
-    t.string "branch_id"
-    t.string "consultant_id"
-    t.string "sales_manager_id"
-    t.string "is_super_user"
-    t.string "block_mail"
-    t.string "allow_book_under_cancellation"
-    t.string "updateflag"
-    t.string "created_by"
-    t.string "website"
-    t.string "ukey"
-    t.string "profile"
-    t.string "account_email"
-    t.string "send_sms"
-    t.string "acc_name"
-    t.string "acc_email"
-    t.string "acc_phone"
-    t.string "man_name"
-    t.string "man_email"
-    t.string "man_phone"
-    t.string "res_name"
-    t.string "res_email"
-    t.string "res_phone"
-    t.string "agency_email"
-    t.string "payment_type"
-    t.string "remarks"
-    t.string "accounts_processed"
-    t.string "site_id"
-    t.string "wh_agent"
-    t.string "wh_main_agent"
-    t.string "last_login_date"
-    t.string "apply_service_tax"
-    t.string "allow_quotation"
-    t.string "is_xmlout"
-    t.string "ip_address"
-  end
-
-  create_table "costsheets", force: :cascade do |t|
-    t.integer "booking_id"
-    t.string  "package_id"
-    t.integer "agent_id"
-    t.string  "packagename"
-    t.string  "cities"
-    t.string  "ratings"
-    t.string  "stay_days"
-    t.string  "pkg_start_date"
-    t.string  "pkg_end_date"
-    t.integer "noofrooms"
-    t.integer "noofadults"
-    t.integer "noofchild"
-    t.string  "create_date"
-    t.float   "total_rate"
-    t.float   "original_rate"
-    t.string  "discount"
-    t.string  "sel_service"
-    t.string  "package_type"
-    t.float   "final_rate"
-    t.string  "agency_name"
-  end
 
   create_table "daywise_booking_sources", force: :cascade do |t|
     t.integer "branch_id"
@@ -142,15 +42,6 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.integer "tkt_count"
   end
 
-  create_table "ticket_vs_times", force: :cascade do |t|
-    t.string  "branch_name"
-    t.string  "fin_year"
-    t.string  "month"
-    t.integer "week"
-    t.integer "day"
-    t.integer "tickets_booked"
-  end
-
   create_table "tkt_cnt_daywises", force: :cascade do |t|
     t.integer "branch_id"
     t.integer "fin_year"
@@ -158,6 +49,8 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.string  "week"
     t.string  "day"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_tkt_cnt_daywises_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_tkt_cnt_daywises_on_fin_year", using: :btree
   end
 
   create_table "tkt_cnt_monthwises", force: :cascade do |t|
@@ -165,6 +58,8 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.integer "fin_year"
     t.string  "month"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_tkt_cnt_monthwises_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_tkt_cnt_monthwises_on_fin_year", using: :btree
   end
 
   create_table "tkt_cnt_weekwises", force: :cascade do |t|
@@ -173,12 +68,16 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.string  "month"
     t.string  "week"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_tkt_cnt_weekwises_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_tkt_cnt_weekwises_on_fin_year", using: :btree
   end
 
   create_table "tkt_cnt_yearwises", force: :cascade do |t|
     t.integer "branch_id"
     t.integer "fin_year"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_tkt_cnt_yearwises_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_tkt_cnt_yearwises_on_fin_year", using: :btree
   end
 
   create_table "weekwise_booking_sources", force: :cascade do |t|
@@ -188,6 +87,9 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.string  "week"
     t.integer "booking_src_id"
     t.integer "tkt_count"
+    t.index ["booking_src_id"], name: "index_weekwise_booking_sources_on_booking_src_id", using: :btree
+    t.index ["branch_id"], name: "index_weekwise_booking_sources_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_weekwise_booking_sources_on_fin_year", using: :btree
   end
 
   create_table "weekwise_peekday_weekdays", force: :cascade do |t|
@@ -198,6 +100,8 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.string  "day"
     t.integer "category_id"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_weekwise_peekday_weekdays_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_weekwise_peekday_weekdays_on_fin_year", using: :btree
   end
 
   create_table "yearwise_booking_sources", force: :cascade do |t|
@@ -205,6 +109,9 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.integer "fin_year"
     t.integer "booking_src_id"
     t.integer "tkt_count"
+    t.index ["booking_src_id"], name: "index_yearwise_booking_sources_on_booking_src_id", using: :btree
+    t.index ["branch_id"], name: "index_yearwise_booking_sources_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_yearwise_booking_sources_on_fin_year", using: :btree
   end
 
   create_table "yearwise_peekday_weekdays", force: :cascade do |t|
@@ -213,6 +120,8 @@ ActiveRecord::Schema.define(version: 20170105121702) do
     t.integer "category_id"
     t.string  "day"
     t.integer "tkt_count"
+    t.index ["branch_id"], name: "index_yearwise_peekday_weekdays_on_branch_id", using: :btree
+    t.index ["fin_year"], name: "index_yearwise_peekday_weekdays_on_fin_year", using: :btree
   end
 
 end
