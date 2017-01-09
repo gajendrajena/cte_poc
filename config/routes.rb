240c59cdb0fa3ad03192bc42962ab4424a84e9f6
Karthik Shanmugam <kshanmugam@ctepl.com>
2017-01-09 18:13:34 +0530

    - Groupwise Chart with actual data model

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'wonderla_tkt_vs_time' => "home#wonderla_tkt_vs_time"
   get 'peakday_weekday' => "home#peakday_weekday"
   get 'wonderla_tkt_vs_source' => "home#wonderla_tkt_vs_source"
+  get 'grpup_wise' => "home#grpup_wise"
   get 'home/show'
   root to: 'home#wonderla'
 
