444b990fd2b9c38d438a50f860d2610d2b625982
Ravi Shrivastava <rsrivastava@ctepl.com>
2017-01-09 14:35:24 +0530

    - Added monthwise index for source

--- app/models/monthwise_booking_source.rb
+++ app/models/monthwise_booking_source.rb
@@ -5,16 +5,7 @@ class MonthwiseBookingSource < ApplicationRecord
 		query_data =  branch_id.present? ? where(fin_year: year, branch_id: branch_id) : where(fin_year: year)
 		data = query_data.group_by(&:booking_src_id).collect{ |k,v| {name: BOOKING_SRC[k.to_s], data: v.group_by(&:month).collect{|k,v| v.map(&:tkt_count).sum}}}
 		
-		{ chart_data: data.to_json, categories: MONTHS, title: format_year(year)}
-	end
-
-	def self.format_year(year)
-	" : " + year.to_s.slice(0,4) + '-' + year.to_s.slice(4,4)
-	end
-
-
-	def self.month(year)
-		MONTHS.collect{|m| m}
+		{ chart_data: data.to_json, categories: MONTHS, title: TktCntMonthwise.format_year(year)}
 	end
 
 end
