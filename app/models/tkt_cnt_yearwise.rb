142dfdb6f1623640faa28d7f09c7c60fe972f80b
Gajendra Jena <gjena@ctepl.com>
2017-01-11 13:43:32 +0530

    sorting year data

--- app/models/tkt_cnt_yearwise.rb
+++ app/models/tkt_cnt_yearwise.rb
@@ -1,33 +1,45 @@
 class TktCntYearwise < ApplicationRecord
 
-	attr_accessor :text
-	def self.fin_years
-		select('id,fin_year').where(branch_id: 1).collect{|tyw| [ tyw.fin_year_text, tyw.fin_year]}
-	end
+  attr_accessor :text
+  def self.fin_years
+    select('id,fin_year').where(branch_id: 1).collect{|tyw| [ tyw.fin_year_text, tyw.fin_year]}
+  end
 
-	def fin_year_text
-		self.fin_year.to_s.slice(0,4) + '-' + self.fin_year.to_s.slice(4,4)
-	end
+  def fin_year_text
+    self.fin_year.to_s.slice(0,4) + '-' + self.fin_year.to_s.slice(4,4)
+  end
 
-	def self.fin_year_text(year)
-		year.to_s.slice(0,4) + '-' + year.to_s.slice(6,2)
-	end
+  def self.fin_year_text(year)
+    year.to_s.slice(0,4) + '-' + year.to_s.slice(4,4)
+  end
 
-	def self.yearly_data(branch_id)
-		data = []
-		if branch_id.present?
-			query_data = where(branch_id: branch_id)
-			data = query_data.group_by(&:branch_id).collect{ |k,v| {name: branch_name(k), data: v.collect{|tcy| tcy.tkt_count}}}
-		else
-			query_data = all
-			data = query_data.group_by(&:branch_id).collect{ |k,v| {name: branch_name(k), data: v.collect{|tcy| tcy.tkt_count}}}
-		end
-		pie_data = query_data.group_by(&:fin_year).collect{ |k,v| {name: fin_year_text(k), y: v.sum{|tcy| tcy.tkt_count}}}
-		{ chart_data: data, categories: fin_years.collect{|v| v[0]}, title: '', pie: { data: pie_data}}
-	end
+  def self.yearly_data(branch_id)
+    query_data = if branch_id.present?
+      where(branch_id: branch_id)
+    else
+      all
+    end
+    data = query_data.group_by(&:branch_id).collect{ |k,v| {name: branch_name(k), data: sort_yearwise_data(v)}}
 
-	def self.branch_name(id)
-		BRANCHES.detect{|b| b[:id] == id}[:name]
-	end
+    pie_data = query_data.group_by(&:fin_year).collect{ |k,v| {name: fin_year_text(k), y: v.sum{|tcy| tcy.tkt_count}}}
+    { chart_data: data, categories: sorted_financial_years, title: '', pie: { data: pie_data}}
+  end
+
+  def self.branch_name(id)
+    BRANCHES.detect{|b| b[:id] == id}[:name]
+  end
+
+  def self.sort_yearwise_data(records)
+    financial_years = sorted_financial_years
+    data_list = Array.new(financial_years.length, 0)
+    records.group_by(&:fin_year).collect do |k,v|
+      data_list[financial_years.index(fin_year_text(k))] = v.map(&:tkt_count).sum
+    end
+    data_list
+  end
+
+  def self.sorted_financial_years
+    fin_years.collect{|v|v[0]}.sort
+  end
 
 end
