e72a413c3474091a61bbdab8ff864538128b068b
Karthik Shanmugam <kshanmugam@ctepl.com>
2017-01-11 14:05:00 +0530

    - Yearwise sorting for all the charts

--- app/models/groupwise_yearwise_tkt_cnt.rb
+++ app/models/groupwise_yearwise_tkt_cnt.rb
@@ -3,8 +3,8 @@ class GroupwiseYearwiseTktCnt < ApplicationRecord
 	def self.yearly_data(branch_id)
 		data = []
 		query_data = branch_id.present? ? where(branch_id: branch_id) : all
-		data = query_data.group_by(&:group_id).collect{ |k,v| {name: BOOKING_GROUP[k.to_s], data: v.group_by(&:fin_year).collect{|k,v| v.map(&:tkt_count).sum}}}
-		{ chart_data: data.to_json, categories: TktCntYearwise.fin_years.collect{|v| v[0]}, title: ''}
+		data = query_data.group_by(&:group_id).collect{ |k,v| {name: BOOKING_GROUP[k.to_s], data: TktCntYearwise.sort_yearwise_data(v)}}
+		{ chart_data: data.to_json, categories: TktCntYearwise.sorted_financial_years, title: ''}
 	end
 
 end
