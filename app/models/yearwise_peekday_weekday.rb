07c695ad58a98412afda84d4dd6ab4493a4e5c1d
Karthik Shanmugam <kshanmugam@ctepl.com>
2017-01-11 13:43:56 +0530

    - Peakday Weekday yearwise chart order by Year

--- app/models/yearwise_peekday_weekday.rb
+++ app/models/yearwise_peekday_weekday.rb
@@ -2,15 +2,15 @@ class YearwisePeekdayWeekday < ApplicationRecord
 	class << self
 		def yearwise(branch_id)
 			if branch_id.present?
-				peakday_child_data = where(day: 1, category_id: 2, branch_id: branch_id).group('fin_year').sum(:tkt_count).values
-				peakday_adult_data = where(day: 1, category_id: 1, branch_id: branch_id).group('fin_year').sum(:tkt_count).values
-				weekday_child_data = where(day: 2, category_id: 2, branch_id: branch_id).group('fin_year').sum(:tkt_count).values
-				weekday_adult_data = where(day: 2, category_id: 1, branch_id: branch_id).group('fin_year').sum(:tkt_count).values
+				peakday_child_data = where(day: 1, category_id: 2, branch_id: branch_id).group('fin_year').order("fin_year").order("fin_year").sum(:tkt_count).values
+				peakday_adult_data = where(day: 1, category_id: 1, branch_id: branch_id).group('fin_year').order("fin_year").sum(:tkt_count).values
+				weekday_child_data = where(day: 2, category_id: 2, branch_id: branch_id).group('fin_year').order("fin_year").sum(:tkt_count).values
+				weekday_adult_data = where(day: 2, category_id: 1, branch_id: branch_id).group('fin_year').order("fin_year").sum(:tkt_count).values
 			else
-				peakday_child_data = where(day: 1, category_id: 2).group('fin_year').sum(:tkt_count).values
-				peakday_adult_data = where(day: 1, category_id: 1).group('fin_year').sum(:tkt_count).values
-				weekday_child_data = where(day: 2, category_id: 2).group('fin_year').sum(:tkt_count).values
-				weekday_adult_data = where(day: 2, category_id: 1).group('fin_year').sum(:tkt_count).values
+				peakday_child_data = where(day: 1, category_id: 2).group('fin_year').order("fin_year").sum(:tkt_count).values
+				peakday_adult_data = where(day: 1, category_id: 1).group('fin_year').order("fin_year").sum(:tkt_count).values
+				weekday_child_data = where(day: 2, category_id: 2).group('fin_year').order("fin_year").sum(:tkt_count).values
+				weekday_adult_data = where(day: 2, category_id: 1).group('fin_year').order("fin_year").sum(:tkt_count).values
 			end
 			data = [ {name: 'Child (Peakday)', data: peakday_child_data , stack: 'Peakday'}, 
 				{name: 'Adult (Peakday)', data: peakday_adult_data, stack: 'Peakday'}, 
